7a05053ae9949f3c4208ae999e380559c55e77bc
Filippo Costa <filippo@neysofu.me>
2023-02-14 01:31:27 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/neysofu/go-names/v2
+
+go 1.20
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/mailgun/mailgun-go/v4 v4.8.2 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/mailgun/mailgun-go/v4"
+)
+
+const urlToMonitor = "https://sites.google.com/view/bigdatafelician/website-monitoring"
+
+const emailSender = "filippo@mg.neysofu.me"
+const emailRecipient = "filippo@neysofu.me"
+const mailgunDomain = "mg.neysofu.me"
+
+const cooloffPeriod = 4 * time.Second
+
+func main() {
+	mailgunApiKey := os.Getenv("MAILGUN_API_KEY")
+	fmt.Println("Mailgun API Key: ", mailgunApiKey[:4], "...")
+	numSeenNames := 3
+
+	for {
+		names, _ := findPeopleNames(urlToMonitor)
+		fmt.Println("Found", len(names), "names")
+
+		for _, name := range names[numSeenNames:] {
+			fmt.Println("Found a new name: ", name)
+			go sendEmail(mailgunApiKey, name)
+		}
+		numSeenNames = len(names)
+
+		time.Sleep(cooloffPeriod)
+	}
+}
+
+func findPeopleNames(url string) ([]string, error) {
+	// Request the HTML page.
+	res, err := http.Get(url)
+	if err != nil {
+		return []string{}, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		return []string{}, fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return []string{}, err
+	}
+
+	names := []string{}
+	doc.Find("ul li p").Each(func(i int, s *goquery.Selection) {
+		names = append(names, s.Text())
+	})
+
+	return names, nil
+}
+
+func sendEmail(mailgunApiKey string, subject string) {
+	mg := mailgun.NewMailgun(mailgunDomain, mailgunApiKey)
+	body := "Hello from Mailgun Go!"
+
+	// The message object allows you to add attachments and Bcc recipients
+	message := mg.NewMessage(emailSender, subject, body, emailRecipient)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	// Send the message with a 10 second timeout
+	resp, id, err := mg.Send(ctx, message)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("ID: %s Resp: %s\n", id, resp)
+}
